efca3f8bfea993f3b73c6942b5a79121bb7449a7
Angelo Sciarra <angelosciarra@ymail.com>
2021-04-09 00:06:35 +0200

    added 2 missing tests

--- Java/src/test/java/com/gildedrose/GildedRosesCharacterizationTest.java
+++ Java/src/test/java/com/gildedrose/GildedRosesCharacterizationTest.java
@@ -56,6 +56,19 @@ public class GildedRosesCharacterizationTest {
     }
 
     @Test
+    @DisplayName("Aged Brie with quality 50 and sellIn under 0")
+    void brie4() {
+        Item item = new Item("Aged Brie", -1, 50);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(-2, item.sellIn);
+        assertEquals(50, item.quality);
+    }
+
+
+    @Test
     @DisplayName("Backstage pass with quality over 50")
     void backstage1() {
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50);
@@ -157,6 +170,18 @@ public class GildedRosesCharacterizationTest {
 
         assertEquals(-2, item.sellIn);
         assertEquals(0, item.quality);
+    }
+    @Test
+    @DisplayName("Backstage pass sellIn temporal coupling")
+    void backstage9() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 24);
+
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(10, item.sellIn);
+        assertEquals(25, item.quality);
     }
 
     @Test
