1908fede3a626c0c397e1fcef59a0bddb19b59ec
Angelo Sciarra <angelosciarra@ymail.com>
2021-04-08 20:10:29 +0200

    covered the backstage pass case

--- Java/src/test/java/com/gildedrose/GildedRosesCharacterizationTest.java
+++ Java/src/test/java/com/gildedrose/GildedRosesCharacterizationTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,7 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GildedRosesCharacterizationTest {
 
     @Test
-    void sulfurusSellIn() {
+    @DisplayName("Sulfuras stay unchanged")
+    void sulfuras() {
         Item item = new Item("Sulfuras, Hand of Ragnaros", 10, 10);
         GildedRose gildedRose = new GildedRose(new Item[]{item});
 
@@ -18,7 +20,8 @@ public class GildedRosesCharacterizationTest {
     }
 
     @Test
-    void brieQualityBiggerThanOrEqualTo50() {
+    @DisplayName("Aged Brie with quality over 50 and sellIn over 0")
+    void brie1() {
         Item item = new Item("Aged Brie", 10, 50);
         GildedRose gildedRose = new GildedRose(new Item[]{item});
 
@@ -29,7 +32,8 @@ public class GildedRosesCharacterizationTest {
     }
 
     @Test
-    void brieQualityLessThan50() {
+    @DisplayName("Aged Brie with quality under 50 and sellIn over 0")
+    void brie2() {
         Item item = new Item("Aged Brie", 10, 48);
         GildedRose gildedRose = new GildedRose(new Item[]{item});
 
@@ -40,7 +44,8 @@ public class GildedRosesCharacterizationTest {
     }
 
     @Test
-    void brieQualityLessThan50AndSellInLessThan0() {
+    @DisplayName("Aged Brie with quality under 50 and sellIn under 0")
+    void brie3() {
         Item item = new Item("Aged Brie", -1, 48);
         GildedRose gildedRose = new GildedRose(new Item[]{item});
 
@@ -50,4 +55,107 @@ public class GildedRosesCharacterizationTest {
         assertEquals(50, item.quality);
     }
 
+    @Test
+    @DisplayName("Backstage pass with quality over 50")
+    void backstage1() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50);
+
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(9, item.sellIn);
+        assertEquals(50, item.quality);
+    }
+
+    @Test
+    @DisplayName("Backstage pass with quality under 50 and sellIn over 10")
+    void backstage2() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 49);
+
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(10, item.sellIn);
+        assertEquals(50, item.quality);
+    }
+
+    @Test
+    @DisplayName("Backstage pass with quality 49 and sellIn between 6 and 10")
+    void backstage3() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 6, 49);
+
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(5, item.sellIn);
+        assertEquals(50, item.quality);
+    }
+
+    @Test
+    @DisplayName("Backstage pass with quality under 49 and sellIn between 6 and 10")
+    void backstage4() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 6, 48);
+
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(5, item.sellIn);
+        assertEquals(50, item.quality);
+    }
+
+    @Test
+    @DisplayName("Backstage pass with quality 49 and sellIn under 6")
+    void backstage5() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 48);
+
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(4, item.sellIn);
+        assertEquals(50, item.quality);
+    }
+
+    @Test
+    @DisplayName("Backstage pass with quality 48 and sellIn under 6")
+    void backstage6() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 48);
+
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(4, item.sellIn);
+        assertEquals(50, item.quality);
+    }
+
+    @Test
+    @DisplayName("Backstage pass with quality under 48 and sellIn under 6")
+    void backstage7() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 14);
+
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(4, item.sellIn);
+        assertEquals(17, item.quality);
+    }
+
+    @Test
+    @DisplayName("Backstage pass with sellIn below 0")
+    void backstage8() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", -1, 14);
+
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(-2, item.sellIn);
+        assertEquals(0, item.quality);
+    }
 }
