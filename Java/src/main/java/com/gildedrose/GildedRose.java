9f033cc5761d757524f6dbfc9d110e33230b9a09
Angelo Sciarra <angelosciarra@ymail.com>
2021-04-09 22:31:25 +0200

    isolated sulfuras case

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,6 +14,9 @@ class GildedRose {
     }
 
     private void updateItem(Item item) {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
+        }
         if (item.name.equals("Aged Brie")) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
@@ -36,15 +39,11 @@ class GildedRose {
             }
         } else {
             if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
+                item.quality = item.quality - 1;
             }
         }
 
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
+        item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
             if (item.name.equals("Aged Brie")) {
@@ -55,9 +54,7 @@ class GildedRose {
                 item.quality = 0;
             } else {
                 if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
+                    item.quality = item.quality - 1;
                 }
 
             }
