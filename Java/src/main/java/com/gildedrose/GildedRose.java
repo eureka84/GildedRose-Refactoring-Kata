58aaaafd0b5e35df1fc5fc2648010cf7e16cd618
Angelo Sciarra <angelosciarra@ymail.com>
2021-04-09 23:49:43 +0200

    extracted sulfuras UpdateItem

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -6,12 +6,14 @@ class GildedRose {
     private final UpdateItem updateAgedBrieItem;
     private final UpdateItem updateBackstageItem;
     private final UpdateItem updateGenericItem;
+    private final UpdateItem sulfurasUpdateItem;
 
     public GildedRose(Item[] items) {
         this.items = items;
         updateAgedBrieItem = new UpdateAgedBrieItem();
         updateBackstageItem = new UpdateBackstageItem();
         updateGenericItem = new UpdateGenericItem();
+        sulfurasUpdateItem = new SulfurasUpdateItem();
     }
 
     public void updateQuality() {
@@ -21,17 +23,15 @@ class GildedRose {
     }
 
     private void updateItem(Item item) {
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            return;
-        }
-        if (updateAgedBrieItem.canHandle(item)) {
+        if (sulfurasUpdateItem.canHandle(item)) {
+            sulfurasUpdateItem.handle(item);
+        }else if (updateAgedBrieItem.canHandle(item)) {
             updateAgedBrieItem.handle(item);
         } else if (updateBackstageItem.canHandle(item)) {
             updateBackstageItem.handle(item);
         } else if(updateGenericItem.canHandle(item)) {
             updateGenericItem.handle(item);
         }
-
     }
 
 }
