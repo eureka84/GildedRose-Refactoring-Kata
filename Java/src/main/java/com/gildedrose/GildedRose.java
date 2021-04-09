5852792049f5957683e08e2e4f37fc218cabd283
Angelo Sciarra <angelosciarra@ymail.com>
2021-04-09 23:54:21 +0200

    completed refactoring

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -3,17 +3,17 @@ package com.gildedrose;
 class GildedRose {
     Item[] items;
 
-    private final UpdateItem updateAgedBrieItem;
-    private final UpdateItem updateBackstageItem;
-    private final UpdateItem updateGenericItem;
-    private final UpdateItem sulfurasUpdateItem;
+    private final UpdateItem[] updateItems;
 
     public GildedRose(Item[] items) {
         this.items = items;
-        updateAgedBrieItem = new UpdateAgedBrieItem();
-        updateBackstageItem = new UpdateBackstageItem();
-        updateGenericItem = new UpdateGenericItem();
-        sulfurasUpdateItem = new SulfurasUpdateItem();
+
+        updateItems = new UpdateItem[]{
+                new SulfurasUpdateItem(),
+                new UpdateAgedBrieItem(),
+                new UpdateBackstageItem(),
+                new UpdateGenericItem()
+        };
     }
 
     public void updateQuality() {
@@ -23,14 +23,11 @@ class GildedRose {
     }
 
     private void updateItem(Item item) {
-        if (sulfurasUpdateItem.canHandle(item)) {
-            sulfurasUpdateItem.handle(item);
-        }else if (updateAgedBrieItem.canHandle(item)) {
-            updateAgedBrieItem.handle(item);
-        } else if (updateBackstageItem.canHandle(item)) {
-            updateBackstageItem.handle(item);
-        } else if(updateGenericItem.canHandle(item)) {
-            updateGenericItem.handle(item);
+        for (UpdateItem updateItem : updateItems) {
+            if (updateItem.canHandle(item)){
+                updateItem.handle(item);
+                return;
+            }
         }
     }
 
