2432d7d5448d4e7ed0b2a09a4f757497b87509a8
Pau Torrents <pau.torrents@glovoapp.com>
2022-11-09 16:57:44 +0100

    invert some ifs

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,7 @@
 package com.gildedrose;
 
 class GildedRose {
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -10,7 +11,7 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].isAgedBrie()
-                    && !items[i].isBackstagePass()) {
+                && !items[i].isBackstagePass()) {
                 if (items[i].quality > 0) {
                     if (!items[i].isSulfuras()) {
                         items[i].quality = items[i].quality - 1;
@@ -41,19 +42,17 @@ class GildedRose {
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].isAgedBrie()) {
-                    if (!items[i].isBackstagePass()) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].isSulfuras()) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
+                if (items[i].isAgedBrie()) {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
+                    }
+                } else if (items[i].isBackstagePass()) {
+                    items[i].quality = items[i].quality - items[i].quality;
+                } else {
+                    if (items[i].quality > 0) {
+                        if (!items[i].isSulfuras()) {
+                            items[i].quality = items[i].quality - 1;
+                        }
                     }
                 }
             }
